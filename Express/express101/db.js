4e02ddd6d455272262e0bb3f2e83fb7c63521c0a
John Kearney <John.d.kearney1@gmail.com>
2021-07-19 02:21:11 -0500

    caught up on missed friday express lecture

---
+++ Express/express101/db.js
@@ -0,0 +1,23 @@
+const db = {
+  // friends: ['Joey', 'Chandler', 'Monika', 'Fibi', 'Ross', 'Rachel']
+  friends: [
+    {
+      name: 'Tony Stark',
+      handle: 'tincan'
+    },
+    {
+      name: 'Joey',
+      handle: 'ItalionStallion'
+    },
+    {
+      name: 'Monika',
+      handle: 'daBaker'
+    },
+    {
+      name: 'Ross',
+      handle: 'museumnerd129'
+    }
+  ]
+}
+
+module.exports = db;
--- Express/express101/server.js
+++ Express/express101/server.js
@@ -1,29 +1,83 @@
-
-
-
-
-
-
-
-// Old way of setting up a server
 const http = require('http');
+const express = require('express');
+const db = require('./db');
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url);
-  if(req.url === '/'){
-    res.setHeader('Content-Type', 'text/plain')
-    res.statusCode = 200;
-    res.end('Home Page!');
-  } else if(req.url === '/hello'){
-    res.setHeader('Content-Type', 'text/plain')
-    res.statusCode = 200;
-    res.end('Hello World');
-  } else {
-    res.setHeader('Content-Type', 'text/plain')
-    res.statusCode = 404
-    res.end('Not Found');
-  }
+const app = express();
 
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// tell node to use the express app to handle all of our requests
+const server = http.createServer(app);
+
+// ===============================================================================
+//                                  route handlers
+// ===============================================================================
+app.get('/', (req, res) => {
+  res.send('Home Page');
 })
 
-server.listen(3000, '127.0.0.1')
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api/friends', (req, res) => {
+  // res.json({ pancakes: true }) // notice we are passing an object. That object will get converted to JSON
+  // pancakes will become "pancakes", per the JSON standard
+  res.json(db.friends);
+})
+// Params
+app.get('/api/friends/:handle', (req, res) => {
+  // console.log(typeof req.params.handle);
+  // console.log('TEST CLG');
+  // res.json(req.params.handle);
+  const foundFriend = db.friends.find(eachFriend => eachFriend.name.toLowerCase() === req.params.handle.toLowerCase());
+  // console.log(foundFriend);
+  if(!foundFriend){
+    res.status(404);
+    res.json('Friend not found :(');
+  } else {
+    res.json(foundFriend);
+  }
+})
+
+app.get('/*', (req, res) => {
+  res.status(404);
+  res.send('Not Found - Try again');
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Sever running at http://${hostname}:${port}/`);
+})
+
+
+
+
+
+
+
+
+// ================================
+// Old way of setting up a server
+// ================================
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   // console.log(req.url);
+//   if(req.url === '/'){
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.statusCode = 200;
+//     res.end('Home Page!');
+//   } else if(req.url === '/hello'){
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.statusCode = 200;
+//     res.end('Hello World');
+//   } else {
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.statusCode = 404
+//     res.end('Not Found');
+//   }
+
+// })
+
+// server.listen(3000, '127.0.0.1')
