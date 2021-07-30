77a912f833d7c8d7ce6c958abd3585931de29a65
John Kearney <johnkearneydev@gmail.com>
2021-07-30 16:01:27 -0500

    end of day - reviewed sequelize stuff

---
+++ Sequelize/practice/sequelize-practice/config/config.bash
@@ -0,0 +1,3 @@
+npx sequelize-cli model:generate --name User --attributes "firstname:string,lastName:string,email:string,password:string"
+
+npx sequelize-cli model:generate --name Wishlist --attributes "testColumn:string"
